89d22177299d53eb89daf49cf40f2141509bec47
Piotr Bednarek <piotrbednarek99@gmail.com>
2023-03-22 20:27:21 +0100

    Add autotagging to AWS resources

--- src/__main__.py
+++ src/__main__.py
@@ -3,9 +3,13 @@
 import pulumi
 import pulumi_aws as aws
 
+from autotag import register_auto_tags
+
+# Inject tags to existing resources
+register_auto_tags({"PROJECT": "WeatherAlertingSystem"})
+
 # Create an AWS resource (S3 Bucket)
 bucket = aws.s3.Bucket(
     "163636840347-my-test-bucket",
     acl="private",
-    tags={"PROJECT": "WeatherAlertingSystem"},
 )

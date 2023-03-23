48bce01e00d67d1c7ba73702308684ac7f2a21dc
Dawid Karoński <dawid.karonski@orange.com>
2023-03-23 20:40:31 +0100

    Add frontend policy creation by pulumi, add config vars

--- src/frontend_s3.py
+++ src/frontend_s3.py
@@ -3,13 +3,17 @@
 import pulumi
 import pulumi_aws as aws
 
+config = pulumi.Config()
+account_id = config.require("account_id")
+frontend_config = pulumi.Config("frontend")
+
 # Create an AWS resource (S3 Bucket)
 def create_frontend_bucket():
     bucket = aws.s3.Bucket(
-        "163636840347-weather-alerting-frontend",
-        bucket="163636840347-weather-alerting-frontend",
+        f"{account_id}-weather-alerting-frontend",
+        bucket=f"{account_id}-weather-alerting-frontend",
         acl="public-read",
-        policy=(lambda path: open(path).read())("frontend_s3_policy.json"),
+        policy=create_s3_frontend_policy(),
         website=aws.s3.BucketWebsiteArgs(
             index_document="index.html",
             routing_rules="""[{
@@ -22,7 +26,24 @@ def create_frontend_bucket():
                 }]
                 """,
         ),
-        force_destroy=True,
+        force_destroy=frontend_config.require_bool("s3_force_destroy"),
         tags={"PROJECT": "WeatherAlertingSystem"},
     )
     return bucket
+
+
+def create_s3_frontend_policy():
+    frontend_public_get_object_policy = aws.iam.get_policy_document(
+        statements=[
+            aws.iam.GetPolicyDocumentStatementArgs(
+                sid="PublicReadGetObject",
+                actions=[
+                    "s3:GetObject",
+                ],
+                effect="Allow",
+                principals=[aws.iam.GetPolicyDocumentStatementPrincipalArgs(type="*", identifiers=["*"])],
+                resources=[f"arn:aws:s3:::{account_id}-weather-alerting-frontend/*"],
+            )
+        ]
+    ).json
+    return frontend_public_get_object_policy
